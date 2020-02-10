87d147b42ab46bbf97b36bead03daeebdb8ebbe4
livanskyi <a.livanskiy@gmail.com>
2020-02-10 12:55:46 +0200

    adding new file: UnetLogoInsertion.py

---
+++ UnetLogoInsertion.py
@@ -0,0 +1,28 @@
+from BannerReplacer import BannerReplacer
+
+
+
+
+
+
+class UnetLogoInsertion(BannerReplacer):
+    """
+    The model detects banner are and replace it with other logo using Unet neural network model
+    """
+    def __init__(self):
+        pass
+
+    def build_model(self):
+        pass
+
+    def detect_banner(self):
+        pass
+
+    def insert_logo(self):
+        pass
+
+
+
+
+
+
