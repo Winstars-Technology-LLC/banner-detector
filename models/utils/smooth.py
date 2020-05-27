2e7904e3298642cb7d344e1af3a183271892f2e6
Yura <Yura14092000@icloud.com>
2020-05-27 17:47:06 +0300

    fix

--- models/utils/smooth.py
+++ models/utils/smooth.py
@@ -37,54 +37,55 @@ def process_mask(mask):
     center_left = None
 
     mask_points = np.argwhere(mask == 1)
+    if mask_points.any():
 
-    max_x = mask_points[mask_points[:, 1].argmax()]
-    min_x = mask_points[mask_points[:, 1].argmin()]
-    top_y = mask_points[mask_points[:, 0].argmin()]
+        max_x = mask_points[mask_points[:, 1].argmax()]
+        min_x = mask_points[mask_points[:, 1].argmin()]
+        top_y = mask_points[mask_points[:, 0].argmin()]
 
-    if abs(max_x[0] - top_y[0]) > 10 and abs(min_x[0] - top_y[0]) > 10:
+        if abs(max_x[0] - top_y[0]) > 10 and abs(min_x[0] - top_y[0]) > 10:
 
-        left_y_shift, left_x_shift = np.abs(top_y - min_x)
-        right_y_shift, right_x_shift = np.abs(top_y - max_x)
+            left_y_shift, left_x_shift = np.abs(top_y - min_x)
+            right_y_shift, right_x_shift = np.abs(top_y - max_x)
 
-        left_angle = np.arctan(left_y_shift / left_x_shift) * 180 / np.pi
-        right_angle = np.arctan(right_y_shift / right_x_shift) * 180 / np.pi
+            left_angle = np.arctan(left_y_shift / left_x_shift) * 180 / np.pi
+            right_angle = np.arctan(right_y_shift / right_x_shift) * 180 / np.pi
 
-        if left_angle < right_angle:
-            mask[:, top_y[1]:] = 0
-        else:
-            mask[:, :top_y[1]] = 0
-
-    _, mask_contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    first_cnt = True
-    for msk_cnt in mask_contours:
-        if cv2.contourArea(msk_cnt) > np.product(mask.shape) * 0.0012:
-            rect = cv2.minAreaRect(msk_cnt)
-            box = cv2.boxPoints(rect).astype(np.int)
-            xm, ym = rect[0]
-            if first_cnt:
-                first_cnt = False
-                left_ids = np.argwhere(box[:, 0] < xm).squeeze()
-                left = box[left_ids]
-                right = np.delete(box, np.s_[left_ids], 0)
-                top_left, bot_left = left[left[:, 1].argsort(axis=0)]
-                top_right, bot_right = right[right[:, 1].argsort(axis=0)]
-
-                center_left = xm
-                center_right = xm
+            if left_angle < right_angle:
+                mask[:, top_y[1]:] = 0
             else:
-                left_ids = np.argwhere(box[:, 0] < xm).squeeze()
-                if xm < center_left:
-                    left = box[left_ids]
-                    top_left, bot_left = left[left[:, 1].argsort(axis=0)]
-                    center_left = xm
-                elif xm > center_right:
-                    right = np.delete(box, np.s_[left_ids], 0)
-                    top_right, bot_right = right[right[:, 1].argsort(axis=0)]
-                    center_right = xm
+                mask[:, :top_y[1]] = 0
 
-    if first_cnt:
-        return []
+        _, mask_contours, _ = cv2.findContours(mask.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        first_cnt = True
+        for msk_cnt in mask_contours:
+            if cv2.contourArea(msk_cnt) > np.product(mask.shape) * 0.0012:
+                rect = cv2.minAreaRect(msk_cnt)
+                box = cv2.boxPoints(rect).astype(np.int)
+                xm, ym = rect[0]
+                if first_cnt:
+                    first_cnt = False
+                    left_ids = np.argwhere(box[:, 0] < xm).squeeze()
+                    left = box[left_ids]
+                    right = np.delete(box, np.s_[left_ids], 0)
+                    top_left, bot_left = left[left[:, 1].argsort(axis=0)]
+                    top_right, bot_right = right[right[:, 1].argsort(axis=0)]
+
+                    center_left = xm
+                    center_right = xm
+                else:
+                    left_ids = np.argwhere(box[:, 0] < xm).squeeze()
+                    if xm < center_left:
+                        left = box[left_ids]
+                        top_left, bot_left = left[left[:, 1].argsort(axis=0)]
+                        center_left = xm
+                    elif xm > center_right:
+                        right = np.delete(box, np.s_[left_ids], 0)
+                        top_right, bot_right = right[right[:, 1].argsort(axis=0)]
+                        center_right = xm
+
+        if first_cnt:
+            return []
 
         # right_height = bot_right[1] - top_right[1]
         # left_height = bot_left[1] - top_left[1]
@@ -102,9 +103,9 @@ def process_mask(mask):
         #     for (x, y) in zip(restoring, top_y):
         #         mask[y:y + left_height, x] = 1
 
-    mask_points = [*top_left, *top_right, *bot_left, *bot_right]
+        mask_points = [*top_left, *top_right, *bot_left, *bot_right]
 
-    return [mask, mask_points]
+        return [mask, mask_points]
 
 
 def smooth_series(series):
